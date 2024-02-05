8138cd82a193e5554357bd2eba432c05b06e2ba9
Oren Pinkas <oren@cherrypick-consulting.com>
2024-02-05 18:34:55 +0200

    added create, update and delete post endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.list_blogs)
+    path("posts/", views.list_posts),
+    path("create/", views.create_post),
+    path("update/<int:pk>/", views.update_post),
+    path("delete/<int:pk>/", views.delete_post),
 ]
--- api/views.py
+++ api/views.py
@@ -5,7 +5,31 @@ from .serializers import PostSerializer
 
 
 @api_view(["GET"])
-def list_blogs(request):
+def list_posts(request):
     posts = Post.objects.all()
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
+
+
+@api_view(["POST"])
+def create_post(request):
+    serializer = PostSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["PATCH"])
+def update_post(request, pk):
+    post = Post.objects.get(id=pk)
+    serializer = PostSerializer(instance=post, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def delete_post(request, pk):
+    post = Post.objects.get(id=pk)
+    post.delete()
+    return Response("Post deleted successfully")
