dec2f4d277ea438ccb5a6cafb256e4745d1e5ae3
Oren Pinkas <oren@cherrypick-consulting.com>
2024-02-06 14:25:20 +0200

    switch get_or_create to bulk_create

--- blog_app/management/commands/seed.py
+++ blog_app/management/commands/seed.py
@@ -3,57 +3,66 @@ from django.core.management.base import BaseCommand
 from django_seed import Seed
 import random
 from faker import Faker
+from blog_app.utils.authors import get_authors
+import time
+
 
 def seed_categories():
-    categories = ["Science", "Technology", "Politics", "Sports", "Health", "Entertainment", "Travel", "Food", "Fashion", "Lifestyle"]
-    categories = map(lambda category: {"name": category}, categories)
+    categories = [
+        "Science",
+        "Technology",
+        "Politics",
+        "Sports",
+        "Health",
+        "Entertainment",
+        "Travel",
+        "Food",
+        "Fashion",
+        "Lifestyle",
+    ]
 
-    categories = [Category.objects.get_or_create(**category)[0] for category in categories]
+    categories = Category.objects.bulk_create(
+        [Category(name=category) for category in categories]
+    )
     return categories
 
+
 def seed_authors():
-    authors = [
-            {
-                "name": "John Doe",
-                "email": "john.doe@blogs.com"
-            },
-            {
-                "name": "Jane Doe",
-                "email": "jane.doe@blogs.com"
-            },
-            {
-                "name": "oren pinkas",
-                "email": "oren@blogs.com"
-            },
-        ]
-    
-    authors = [Author.objects.get_or_create(**author)[0] for author in authors]
+    authors = get_authors()
+
+    authors = Author.objects.bulk_create([Author(**author) for author in authors])
     return authors
 
-def seed_posts(n, all_authors, all_categories):
-    posts = [generate_post(all_authors, all_categories) for _ in range(n)]
-    for title, content, author, categories in posts:
-        post = Post(title=title, content=content, author=author)
-        post.save()
-        post.categories.set(categories)
 
-def generate_post(all_authors, all_categories):
+def seed_posts(n):
+    authors = Author.objects.all()
+    posts = [Post(**generate_post_data(authors)) for _ in range(n)]
+
+    posts = Post.objects.bulk_create(posts)
+
+    categories = list(Category.objects.all())
+    for post in posts:
+        post.categories.set(random.sample(categories, random.randint(1, 3)))
+
+
+def generate_post_data(authors):
     fake = Faker()
-    title = f"{fake.catch_phrase()} {random.choice(['Tips', 'Tricks', 'Guide', 'Ideas'])}"
-    content = fake.text(max_nb_chars=1000)  # Generates random text up to 1000 characters
-    author = random.choice(all_authors)
-    categories = random.choices(all_categories, k=random.randint(1, 3))
-    print(categories)
-    return title, content, author, categories
+    title = (
+        f"{fake.catch_phrase()} {random.choice(['Tips', 'Tricks', 'Guide', 'Ideas'])}"
+    )
+    content = fake.text(
+        max_nb_chars=1000
+    )  # Generates random text up to 1000 characters
+    author = random.choice(authors)
+    return {"title": title, "content": content, "author": author}
 
 
 class Command(BaseCommand):
-    help = 'Seeds the database with initial data'
+    help = "Seeds the database with initial data"
 
     def handle(self, *args, **options):
+        seed_categories()
+        seed_authors()
+        seed_posts(10000)
 
-        authors = seed_authors()
-        categories = seed_categories()
-        seed_posts(12, authors, categories)
-
-        self.stdout.write(self.style.SUCCESS('Successfully seeded the database!'))
+        self.stdout.write(self.style.SUCCESS("Successfully seeded the database!"))
---
+++ blog_app/utils/authors.py
@@ -0,0 +1,41 @@
+import requests
+
+url = "https://randomuser.me/api/"
+
+
+def get_random_author():
+    response = requests.get(url)
+    data = response.json()
+    author = data["results"][0]
+    return {
+        "name": f"{author['name']['first']} {author['name']['last']}",
+        "email": author["email"],
+    }
+
+
+authors = [
+    {"name": "Leif Morgenstern", "email": "leif.morgenstern@example.com"},
+    {"name": "Imre Backes", "email": "imre.backes@example.com"},
+    {"name": "Emilia Wiita", "email": "emilia.wiita@example.com"},
+    {"name": "Keerthi Mugeraya", "email": "keerthi.mugeraya@example.com"},
+    {"name": "Bushra Lundal", "email": "bushra.lundal@example.com"},
+    {"name": "Ricardo Cruz", "email": "ricardo.cruz@example.com"},
+    {"name": "Adolfo Montero", "email": "adolfo.montero@example.com"},
+    {"name": "Justin Wong", "email": "justin.wong@example.com"},
+    {"name": "Olivia Hakala", "email": "olivia.hakala@example.com"},
+    {"name": "Asha Dijkers", "email": "asha.dijkers@example.com"},
+    {"name": "Lisa Barrett", "email": "lisa.barrett@example.com"},
+    {"name": "Byron Gordon", "email": "byron.gordon@example.com"},
+    {"name": "Ashton Thomas", "email": "ashton.thomas@example.com"},
+    {"name": "Illya Lapichak", "email": "illya.lapichak@example.com"},
+    {"name": "Sergio Romero", "email": "sergio.romero@example.com"},
+    {"name": "Elsa Kalm", "email": "elsa.kalm@example.com"},
+    {"name": "Marit Lamens", "email": "marit.lamens@example.com"},
+    {"name": "Ava Lévesque", "email": "ava.levesque@example.com"},
+    {"name": "Chloe Clark", "email": "chloe.clark@example.com"},
+    {"name": "Christine Pena", "email": "christine.pena@example.com"},
+]
+
+
+def get_authors():
+    return authors
