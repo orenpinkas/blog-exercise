9e07bc3edc9818855cbef2c8bd2f8f1cbfc1433f
Oren Pinkas <oren@cherrypick-consulting.com>
2024-02-08 17:18:13 +0200

    added unit tests for urls and models

--- api/tests/test_urls.py
+++ api/tests/test_urls.py
@@ -1,13 +1,35 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from api.views import post_list, post_detail
+from api.PostViews import post_list, post_detail
+from api.AuthorViews import author_list, author_detail
+from api.CategoryViews import category_list, category_detail
 
 
-class TestUrls(SimpleTestCase):
+class TestPostUrls(SimpleTestCase):
     def test_list_url_resolves(self):
         url = reverse("post-list")
         self.assertEqual(resolve(url).func, post_list)
 
-    def test_detail_url(self):
+    def test_detail_url_resolves(self):
         url = reverse("post-detail", args=[1])
         self.assertEqual(resolve(url).func, post_detail)
+
+
+class TestAuthorUrls(SimpleTestCase):
+    def test_list_url_resolves(self):
+        url = reverse("author-list")
+        self.assertEqual(resolve(url).func, author_list)
+
+    def test_detail_url_resolves(self):
+        url = reverse("author-detail", args=[1])
+        self.assertEqual(resolve(url).func, author_detail)
+
+
+class TestCategoryUrls(SimpleTestCase):
+    def test_list_url_resolves(self):
+        url = reverse("category-list")
+        self.assertEqual(resolve(url).func, category_list)
+
+    def test_detail_url_resolves(self):
+        url = reverse("category-detail", args=[1])
+        self.assertEqual(resolve(url).func, category_detail)
--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import PostViews, AuthorViews, CategoryViews
 
 urlpatterns = [
-    path("posts/", PostViews.post_list),
-    path("posts/<int:pk>/", PostViews.post_detail),
-    path("categories/", CategoryViews.category_list),
-    path("categories/<int:pk>/", CategoryViews.category_detail),
-    path("authors/", AuthorViews.author_list),
-    path("authors/<int:pk>/", AuthorViews.author_detail),
+    path("posts/", PostViews.post_list, name="post-list"),
+    path("posts/<int:pk>/", PostViews.post_detail, name="post-detail"),
+    path("categories/", CategoryViews.category_list, name="category-list"),
+    path("categories/<int:pk>/", CategoryViews.category_detail, name="category-detail"),
+    path("authors/", AuthorViews.author_list, name="author-list"),
+    path("authors/<int:pk>/", AuthorViews.author_detail, name="author-detail"),
 ]
